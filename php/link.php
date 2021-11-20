889de1c23c554fa57e18545c87bdacef84b9fad5
vinayakkalra <vinayak.kalra@quadbtech.com>
2021-11-20 13:36:07 +0530

    changes

---
+++ php/link.php
@@ -0,0 +1,11 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "", "finchords");
+
+if (mysqli_connect_error()){
+    die("<script>console.log('There is a problem with mysql connection')</script>");
+}
+
+
+?>
+   
